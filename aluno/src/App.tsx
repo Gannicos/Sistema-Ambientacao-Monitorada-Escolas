98bd84d15eda5c9d299f4b7aba901254ce9e6639
Victor Hugo <vhlima1008@gmail.com>
2024-09-12 17:02:31 -0300

    Criação de componentes da tela de exibição 'aluno'.

--- aluno/src/App.tsx
+++ aluno/src/App.tsx
@@ -1,10 +1,15 @@
 import './App.css'
+import LeftBar from './components/left_bar/left_bar';
+import Map from './components/map/map';
 
 function App() {
 
   return (
     <>
-
+      <div className="modular-page-container">
+        <LeftBar/>
+        <Map/>
+      </div>
     </>
   )
 }
---
+++ aluno/src/components/map/map.tsx
@@ -0,0 +1,10 @@
+import './map.css';
+
+export default function map(){
+
+    return(
+        <div className="map-container">
+            <h3>It's a map</h3>
+        </div>
+    );
+}
